5e6e17cbf25c9b7702a83a163e1f27e1913a3fc4
fayazmohammed07 <54425316+fayazmohammed07@users.noreply.github.com>
2019-09-16 12:38:48 +0530

    Update bootst.sh

--- bootst.sh
+++ bootst.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-#echo "please enter file name"
-#read file
+echo "please enter file name"
+read file
 #echo "please enter file name for node name "
 #read file1
 
 cd /opt/chef-repo
 while read p
 do
-           echo "$q"
+           echo "$p"
            cd /opt/chef-repo
            knife bootstrap $p -U root -P 12345678 --sudo -N "node" -y
 done < file.txt
